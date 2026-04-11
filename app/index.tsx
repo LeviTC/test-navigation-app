f39d50850fd885efbf6c1675a7c38cff99f0efd6
LeviTC <erletaco@outlook.com>
2026-04-11 12:03:01 -0600

    adding nativewind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "../global.css";
+
+export default function Index() {
+  return (
+    <View>
+      <Text
+      className=" bg-red-500"
+      >Hello World</Text>
+    </View>
+  );
+}
